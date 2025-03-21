69357cc0ae764f14b0044fb47469825cfe9134fa
keloggzzz <kelis.clark10@gmail.com>
2025-03-21 12:17:00 -0400

    Search page layout done

--- src/Components/Search.jsx
+++
@@ -1,9 +0,0 @@
-import {useState, useContext} from "react";
-import { DataContext } from "../App";
-
-
-
-export default function Search(){
-   return(<div><h1 className="text-gray-950">This will be search results page</h1></div>)
-
-}
