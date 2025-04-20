a7130b5a8ce1cc87cd4c1a96d1b7553ec4b9c412
keloggzzz <kelis.clark10@gmail.com>
2025-04-20 19:20:11 -0400

    Adding orgpage back

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
-import { faSearch, faHouse, faUser, faUsersRays } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faHouse, faUser, faUsersRays, faBuilding } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function NavBar() {
@@ -42,8 +42,8 @@ export default function NavBar() {
       </div>
 
       {/*******************************************************Link to Home*****************************************************************/}
-      <div className="flex gap-x-30 text-gray-700 text-md font-medium">
-        <div className="flex flex-col items-center mr-5 ml-30">
+      <div className="flex gap-x-15 text-gray-700 text-md font-medium">
+        <div className="flex flex-col items-center mr-5 ml-10">
           <a href="/Home">
             <div className="flex flex-col items-center">
               <FontAwesomeIcon className="py-2" icon={faHouse} />
@@ -74,6 +74,16 @@ export default function NavBar() {
               <FontAwesomeIcon className="py-2" icon={faUsersRays} />
             </div>
             <b>CONNECTIONS</b>
+          </a>
+      </div>
+            {/*******************************************************Link to Connections**********************************************************/}
+
+            <div className="flex flex-col items-center ml-10">
+          <a href="/OrganizationsPage">
+            <div className="flex flex-col items-center">
+              <FontAwesomeIcon className="py-2" icon={faBuilding} />
+            </div>
+            <b>ORGANIZATIONS</b>
           </a>
         </div>
       </div>
