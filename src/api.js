f0c736e7aef57d91e7eb7e717aad9ad938c9cf35
ccubiccrystal <cubiccrystal@proton.me>
2025-05-05 08:07:56 -0400

    Quick fix #2.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: import.meta.env.PROD ? "/" : "http://localhost:8000", // Change this to your backend URL
+	baseURL: import.meta.env.PROD ? "https://bearcat-exp.vercel.app" : "http://localhost:8000", // Change this to your backend URL
     withCredentials: true, // Allows sending cookies (for refresh token)
 });
 
